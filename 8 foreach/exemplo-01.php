60d3014ae1aaf490a408564369eabca7bd602b85
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-12-19 01:17:22 -0300

    Foreach

---
+++ 8 foreach/exemplo-01.php
@@ -0,0 +1,23 @@
+<?php
+
+$meses = array(
+    "Janeiro", "Fevereiro","Março",
+     "Abril", "Maio","Junho",
+      "Julho", "Agosto","Setembro",
+       "Outubro", "Novembro","Dezembro"
+);
+
+foreach ($meses as $mes) {
+    
+    echo "O mês é: ". $mes."<br>";
+}
+
+echo"=================================="."<br>";
+
+foreach ($meses as $index => $mes) {
+    
+    echo "ìndice: ".$index."<br>";
+    echo "O mês é: ". $mes."<br>";
+}
+
+?>
