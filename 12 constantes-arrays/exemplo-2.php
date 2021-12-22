9c6873e551cba0f94e894f82aaf9a1a018ceed5f
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-12-21 22:20:19 -0300

    Altera exemplo 2

--- 12 constantes-arrays/exemplo-2.php
+++ 12 constantes-arrays/exemplo-2.php
@@ -7,10 +7,7 @@ define("BANCO_DE_DADOS", [
     'password',
     'test'
 
-], true);
-
-// Com o parâmetro true deixa case-insensitive, não é comun,
-// melhor não colocar e escrever tudo maisculo
+]);
 
 print_r(BANCO_DE_DADOS)
 
