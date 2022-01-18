c6fc1c47bc236e1d0e1d7f9b78a6f5182b7bcbb4
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-01-18 16:35:56 -0300

    Correcoes e classe usuario

--- 25 dao/class/Usuario.php
+++ 25 dao/class/Usuario.php
@@ -34,6 +34,30 @@ class Usuario {
     public function setDtcadastro($value){
         $this->dtcadastro = $value;
     }
+
+    public function loadById($id){
+        $sql = new Sql();
+        $results = $sql -> select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID"=>$id
+        ));
+
+        if (count($results) > 0){
+            $row = $results[0];
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+    }
+
+    public function __toString(){
+        return json_encode(array(
+            "idusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+        ));
+    }
 }
 
 ?>
