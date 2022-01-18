6ee683cdc2c30434080437be87acd1ef65c06743
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-01-18 15:40:49 -0300

    Criada a pasta class

---
+++ 25 dao/class/Usuario.php
@@ -0,0 +1,39 @@
+<?php
+
+class Usuario {
+
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+
+    public function getIdusuario(){
+        return $this->idusuario;
+    }
+    public function setIdusuario($value){
+        $this->idusuario = $value;
+    }
+
+    public function getDeslogin(){
+        return $this->deslogin;
+    }
+    public function setDeslogin($value){
+        $this->deslogin = $value;
+    }
+
+    public function getDessenha(){
+        return $this->dessenha;
+    }
+    public function setDessenha($value){
+        $this->dessenha = $value;
+    }
+
+    public function getDtcadastro(){
+        return $this->dtcadastro;
+    }
+    public function setDtcadastro($value){
+        $this->dtcadastro = $value;
+    }
+}
+
+?>
--- 25 dao/config.php
+++ 25 dao/config.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function($class_name){
 
-    $filenmae = $class_name.".php";
+    $filenmae = "class".DIRECTORY_SEPARATOR.$class_name.".php";
 
 if(file_exists($filenmae)){
     require_once($filenmae);
