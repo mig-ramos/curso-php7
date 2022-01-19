ec5fee32217336f7afc84577cc0ebf7c687fdd21
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-01-18 22:55:14 -0300

    Metodos de lista

--- 25 dao/index.php
+++ 25 dao/index.php
@@ -6,8 +6,21 @@
   //  $usuarios = $sql->select("SELECT * FROM tb_usuarios");
   //  echo json_encode($usuarios);
 
-  $root = new Usuario();
-  $root->loadById(3);
-  echo $root;
+  // Carrega um usuario
+  //$root = new Usuario();
+  //$root->loadById(3);
+  //echo $root;
 
+  // Carrega uma lista de ususarios
+  //  $lista = Usuario::getList();
+  //  echo json_encode($lista);
+
+  // Carrega uma lista de usuários buscando pelo login
+  //$search = Usuario::search("Joa");
+  //echo json_encode($search);
+
+  //Carrega um usuario usando o login e a senha
+  $usuario = new Usuario();
+  $usuario->login("Jose","1234567890");
+  echo $usuario;
 ?>
