06b1c18e3c93339a338a7ea76a15b4893f1e0e9c
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-02-14 22:04:20 -0300

    ReCaptcha gerar chaves no google

---
+++ 42_seguranca/exemplo-4.php
@@ -0,0 +1,8 @@
+<script src="https://www.google.com/recaptcha/api.js"></script>
+<form action="cadastrar.php" method="post">
+
+    <div class="g-recaptcha" data-sitekey="6LfV2hgUAAAAALCODft3IfKbEdDu_tJNLyRlesYg"></div>
+    <input type="email" name="inputEmail">
+    <button type="submit">Enviar</button>
+
+</form>
